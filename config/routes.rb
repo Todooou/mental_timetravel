c3af438773a6202ddac192ebc1e4498c85195249
erii todo <eriitodo@toutoririkanoMacBook-Pro.local>
2020-07-18 15:23:36 +0900

    Add comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "posts#new"
   resources :posts
+  # routing for users
   resources :users, only: [:show, :edit, :update]
   resources :relationships, only: [:create, :destroy]
   
